8a24f9cfd98e8840b585aec9d31b8d27b492f5d4
mythstreetstore-svg <mythstreetstore@gmail.com>
2026-04-18 10:20:43 +0530

    fix db connection

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "ecommerce_db",
-  password: "postgres123",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
