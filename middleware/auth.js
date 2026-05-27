c600055509ab0b29002be0991b168db5324be88a
mythstreetstore-svg <mythstreetstore@gmail.com>
2026-05-27 19:13:13 +0530

    fix: connect to render postgres using DATABASE_URL

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,10 @@ export default function auth(req, res, next) {
       return res.status(401).json({ error: "No token" });
     }
 
-    const decoded = jwt.verify(token, "secret123");
+    const decoded = jwt.verify(
+  token,
+  process.env.JWT_SECRET
+);
 
     req.user = decoded;
 
